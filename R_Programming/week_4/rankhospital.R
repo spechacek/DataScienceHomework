79c05eac5ccc0b3e01b0e3d91ec7847fcce29098
shane pechacek <shane.pechacek@gmail.com>
2016-02-02 09:20:07 -0600

    started function

--- R_Programming/week_4/rankhospital.R
+++ R_Programming/week_4/rankhospital.R
@@ -1,3 +1,7 @@
-rankhospital<-function(state, outcome){
+rankhospital<-function(state, outcome, num = "best"){
+  
+  rawdata <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  
+  
   
 }
