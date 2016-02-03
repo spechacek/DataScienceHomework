926c899353f0fd490378c2650e96eda5e52ef595
shane pechacek <shane.pechacek@gmail.com>
2016-02-02 19:14:25 -0600

    started working on function

--- R_Programming/week_4/rankhospital.R
+++ R_Programming/week_4/rankhospital.R
@@ -1,7 +1,20 @@
 rankhospital<-function(state, outcome, num = "best"){
   
-  rawdata <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  best_results<-best(state, outcome)
   
+  if(num == "best"){
+    message("best")
+    best_results[1,2]
+  }
   
+  if(num == "worst"){
+    message("worst")
+    best_results[nrow(best_results),2]
+  }
+  
+  if(is.numeric(num)){
+    message("number")
+    best_results[,c(2,17)]
+  }
   
 }
