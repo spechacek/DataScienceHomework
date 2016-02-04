63acda9daa38e5194a177455ec6e5e684bcdfc41
shane pechacek <shane.pechacek@gmail.com>
2016-02-03 19:03:33 -0600

    completed function

--- R_Programming/week_4/rankhospital.R
+++ R_Programming/week_4/rankhospital.R
@@ -2,19 +2,32 @@ rankhospital<-function(state, outcome, num = "best"){
   
   best_results<-best(state, outcome)
   
+  valid_outcomes<-c("heart attack", "heart failure", "pneumonia")
+  #create dataframe for outcome index lookup
+  outcomes_idx<-c(11,17,23)
+  outcomes_lookup<-data.frame(valid_outcomes, outcomes_idx)
+  outcome_idx<-outcomes_lookup[outcomes_lookup$valid_outcomes == outcome,2]
+  
+  ordered_results<-best_results[order(best_results[,outcome_idx], best_results[,2]),]
+  
+  output<-NULL
+  
   if(num == "best"){
-    message("best")
-    best_results[1,2]
+    #message("best")
+    output<-ordered_results[1,2]
   }
   
   if(num == "worst"){
-    message("worst")
-    best_results[nrow(best_results),2]
+    #message("worst")
+    output<-ordered_results[nrow(best_results),2]
   }
   
   if(is.numeric(num)){
-    message("number")
-    best_results[,c(2,17)]
+    #message("number")
+    #ordered_results[,c(2,17)]
+    output<-ordered_results[num,2]
   }
+  
+  output
   
 }
